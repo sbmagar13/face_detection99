6eca19accf4bcf9ff103cf3cbc47c6d4fb347874
SBMagar <sbmagar.sbm99@gmail.com>
2020-10-02 20:37:58 +0545

    Removed all Custom Models

--- rekognition_app.py
+++ rekognition_app.py
@@ -1,10 +1,6 @@
 import logging
 import boto3
-# from botocore.exceptions import ClientError
 import json
-
-# import math
-# import matplotlib.pyplot as plt
 
 
 configfile = 'credentials_config.txt'
@@ -17,39 +13,35 @@ with open(configfile) as cf:
 
 session = boto3.session.Session(profile_name = 'default')
 client1 = session.client('rekognition', aws_access_key_id=access_key_id,
-                      aws_secret_access_key=secret_access_key, aws_session_token=session_token, region_name='us-east-1')
+                                        aws_secret_access_key=secret_access_key,
+                                        aws_session_token=session_token,
+                                        region_name='us-east-1')
 
 
 client2 = session.client('s3', aws_access_key_id=access_key_id,
-                      aws_secret_access_key=secret_access_key, aws_session_token=session_token, region_name='us-east-1')
+                                aws_secret_access_key=secret_access_key,
+                                aws_session_token=session_token,
+                                region_name='us-east-1')
 
 
-def get_image(filename):
-    with open(filename, 'rb') as imgfile:
-        return imgfile.read()
+# def get_image(filename):
+#     with open(filename, 'rb') as imgfile:
+#         return imgfile.read()
 
 
-# def detect_images(images):
-#     fig=plt.figure(figsize=(40,40))
-#     rows = math.ceil((len(images))/4)
-#     columns = math.ceil(len(images)/rows)
-#     for i in range(0, columns*rows):
-#         ax = fig.add_subplot(rows, columns, i+1)
-#         ax.xaxis.set_visible(False)
-#         ax.yaxis.set_visible(False)
-#         if i < len(images):
-#             plt.imshow(images[i])
-#     plt.tight_layout()
-#     plt.show
-
-
-imgb = get_image("Images/ramailo.jpg")
+# imgb = get_image("Images/elon musk.jpg")
+photo = 'ramailo.jpg'
 res1 = client1.detect_faces(
     Image={
-        'Bytes':imgb
+        # 'Bytes':imgb
+        'S3Object': {
+            'Bucket': 'rekognitiondata99',
+            'Name': photo
+        }
     },
     Attributes=['ALL']
 )
+print("Detected faces for " + photo)
 for person in res1['FaceDetails']:
     # print(json.dumps(face, indent=4))
     print('Gender: ' + person['Gender']['Value'] )
@@ -65,26 +57,28 @@ for person in res1['FaceDetails']:
         print('- Has Beard.')
     if person['Mustache']['Value'] == True:
         print('- Has Mustache.')
+    if person['MouthOpen']['Value'] == True:
+        print('- Mouth is wide open!!!')
 
     print ('-----------------------------------')
 
 
 
 res2 = client2.list_buckets()
-print('Existing Buckets:')
+print('Existing Buckets in S3:')
 for bucket in res2['Buckets']:
-    print(f'  {bucket["Name"]}')
+    print(f'{bucket["Name"]}')
 
 
 
 # upload file in bucket:
 #
-# with open('PAS072BCT638.jpg', 'rb') as f:
+# with open('Images/ramailo.jpg', 'rb') as f:
 #     data = f.read()
 # res3 = client2.put_object(
 #     ACL='private',
 #     Body=data,
 #     Bucket='rekognitiondata99',
-#     Key='PAS072BCT638.jpg',
+#     Key='ramailo.jpg',
 # )
 # print('Uploaded successfully!')
